6833c8a928836d4d5f6a0ba5a0772e73658a7fce
Unknown <gabrielhammes@aluno.puc-rio.br>
2018-07-28 14:42:15 -0300

    pessoa classe

---
+++ HackingRio/HackingRio/Model/Pessoa.swift
@@ -0,0 +1,27 @@
+//
+//  Pessoa.swift
+//  HackingRio
+//
+//  Created by Gabriel Almeida Hammes on 28/07/2018.
+//  Copyright © 2018 Giovanni Severo Barros. All rights reserved.
+//
+
+import Foundation
+
+class Pessoa{
+    
+    var nome : String
+    var nascimento : Date
+    var telefone : String
+    var email : String
+    var cpf : String
+    
+    
+    init(nome: String, nascimento : Date, telefone: String, email : String, publisher: String, isbn: String, cover: String, cpf: String){
+        self.nome = nome
+        self.nascimento = nascimento
+        self.telefone = telefone
+        self.email = email
+        self.cpf = cpf
+    }
+}
