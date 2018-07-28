01fb3b941893357e976d38ac73ee00c954639504
Giovanni <giosvro@gmail.com>
2018-07-28 14:13:04 -0300

    hello

---
+++ HackingRio/Pods/Target Support Files/Pods-HackingRio/Pods-HackingRio-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HackingRio : NSObject
+@end
+@implementation PodsDummy_Pods_HackingRio
+@end
---
+++ HackingRio/Pods/Target Support Files/Pods-HackingRioUITests/Pods-HackingRioUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HackingRioUITests : NSObject
+@end
+@implementation PodsDummy_Pods_HackingRioUITests
+@end
