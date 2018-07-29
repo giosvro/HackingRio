1eea0d916ddccc0daa104aa4056abf1ecf730439
Jessica <jessicahollander@aluno.puc-rio.br>
2018-07-29 03:40:40 -0300

    dao

--- HackingRio/HackingRio/Model/Patient.swift
+++ HackingRio/HackingRio/Model/Patient.swift
@@ -12,10 +12,10 @@ class Patient: Person {
     var adress: String
     var appointment: [Appointment]
     
-    init(name: String, birth: Date, telephone: String, email: String, cpf: String, bankAccount: BankAccount, adress: String, appointment: [Appointment]) {
+    init(name: String, age: String, telephone: String, email: String, cpf: String, bankAccount: BankAccount, adress: String, appointment: [Appointment]) {
         self.adress = adress
         self.appointment = appointment
-        super.init(name: name, birth: birth, telephone: telephone, email: email, cpf: cpf, bankAccount: bankAccount)
+        super.init(name: name, age: age, telephone: telephone, email: email, cpf: cpf, bankAccount: bankAccount)
     }
     
     required init(from decoder: Decoder) throws {
